fba76975a18b8d959313673b2d97b2e0300cfb46
Peter Monaco <peter@monacofamily.org>
2015-10-21 21:27:00 -0700

    Improvements

--- SainSmartLCD-Demo/SainSmartLCD-Demo.ino
+++ SainSmartLCD-Demo/SainSmartLCD-Demo.ino
@@ -8,15 +8,15 @@ www.sainsmart.com
 Displays the currently pressed key on the LCD screen.
 
 Key Codes (in left-to-right order):
-
-None   - 0
-Select - 1
-Left   - 2
-Up     - 3
-Down   - 4
-Right  - 5
-
 */
+
+const int KEY_NO_VALUE = -1;
+const int KEY_NONE = 0;
+const int KEY_Select = 1;
+const int KEY_Left = 2;
+const int KEY_Up = 3;
+const int KEY_Down = 4;
+const int KEY_Right = 5;
 
 #include <LiquidCrystal.h>
 #include <DFR_Key.h>
@@ -26,18 +26,14 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 //---------------------------------------------
 
 DFR_Key keypad;
-
-int localKey = 0;
-String keyString = "";
                  
 void setup() 
 { 
-  Serial.begin(9600);
+  //Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Key Grab v0.2");
-  delay(2500);
+  lcd.print("Press a key!");
   
   /*
   OPTIONAL
@@ -49,37 +45,52 @@ void setup()
 
 }
 
-int lastKey = 0;
 void loop() 
-{ 
-  /*
-  keypad.getKey();
-  Grabs the current key.
-  Returns a non-zero integer corresponding to the pressed key,
-  OR
-  Returns 0 for no keys pressed,
-  OR
-  Returns -1 (sample wait) when no key is available to be sampled.
-  */
-  while (true) {
-    localKey = keypad.getKey();
-    if (localKey == -1) {
-      continue;
-    }
-    if (localKey != lastKey) {
-      lastKey = localKey;
-      break;
-    }
-  }
-  Serial.print("BREAK: ");
-  Serial.println(lastKey);
+{
+  int key = nextKeypadKey();
   
-  if (localKey != SAMPLE_WAIT)
-  {
+  if (key != KEY_NO_VALUE) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Current Key:");
     lcd.setCursor(0, 1);
-    lcd.print(localKey);
+    if (key == KEY_NONE) {
+      lcd.print("(none)");
+    } else if (key == KEY_Select) {
+      lcd.print("Select");
+    } else if (key == KEY_Left) {
+      lcd.print("Left");
+    } else if (key == KEY_Up) {
+      lcd.print("Up");
+    } else if (key == KEY_Down) {
+      lcd.print("Down");
+    } else if (key == KEY_Right) {
+      lcd.print("Right");
+    }
   }
 }
+
+
+//
+// Code for reading and debouncing the keypad
+//
+int lastKey = 0;
+int nextKeypadKey() {
+  // keypad.getKey() grabs the current key.
+  // Returns a non-zero integer corresponding to the pressed key, OR
+  // Returns 0 for no keys pressed, OR
+  // Returns -1 (sample wait) when no key is available to be sampled.
+  int currKey = keypad.getKey();
+  if (currKey == -1) {
+    // There is no key available to be sampled
+    return KEY_NO_VALUE;
+  }
+  if (currKey == lastKey) {
+    // No change since last time
+    return KEY_NO_VALUE;
+  }
+  
+  lastKey = currKey;
+  return currKey;
+}
+
